e2e73b46d72863af646b596e9b9f838bb6e18fd5
isaac-wahba <isaac.wahba2511@gmail.com>
2024-04-10 16:34:47 +0200

    add initial tasks list to constants

--- src/store/slices/tasksSlice.ts
+++ src/store/slices/tasksSlice.ts
@@ -2,6 +2,7 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../store";
 import { Task } from "../../types/types";
 import { TASK_STATUS } from "../../enums/common";
+import { initialTasksList } from "../../constants/constants";
 
 interface TasksState {
   tasks: Task[];
@@ -9,57 +10,7 @@ interface TasksState {
 }
 
 const initialState: TasksState = {
-  tasks: [
-    {
-      id: "1",
-      title: "Set up project repository",
-      status: TASK_STATUS.PENDING,
-      details:
-        "Create a new GitHub repository for the project, initialize it with necessary files, and set up version control.",
-    },
-    {
-      id: "2",
-      title: "Design user interface mockups",
-      status: TASK_STATUS.COMPLETED,
-      details:
-        "Create mockups and wireframes for the user interface using tools like Figma or Sketch, considering user experience principles and design best practices.",
-    },
-    {
-      id: "3",
-      title: "Implement responsive layout",
-      status: TASK_STATUS.PENDING,
-      details:
-        "Develop responsive layout and design using HTML and CSS frameworks like Bootstrap or Tailwind CSS, ensuring compatibility across different devices and screen sizes.",
-    },
-    {
-      id: "4",
-      title: "Integrate API endpoints",
-      status: TASK_STATUS.PENDING,
-      details:
-        "Integrate with backend API endpoints to fetch and display data dynamically using technologies like Axios or Fetch API, ensuring proper error handling and data validation.",
-    },
-    {
-      id: "5",
-      title: "Implement authentication",
-      status: TASK_STATUS.COMPLETED,
-      details:
-        "Implement user authentication and authorization features using OAuth 2.0 or JWT (JSON Web Tokens), providing secure access to protected routes and resources.",
-    },
-    {
-      id: "6",
-      title: "Optimize performance",
-      status: TASK_STATUS.PENDING,
-      details:
-        "Optimize website performance by minimizing resource load times, leveraging browser caching, and implementing lazy loading techniques for images and assets.",
-    },
-    {
-      id: "7",
-      title: "Write unit tests",
-      status: TASK_STATUS.COMPLETED,
-      details:
-        "Write unit tests using testing frameworks like Jest or Mocha, covering critical components and functionality to ensure code reliability and maintainability.",
-    },
-  ],
+  tasks: initialTasksList,
   filter: TASK_STATUS.ALL,
 };
 
